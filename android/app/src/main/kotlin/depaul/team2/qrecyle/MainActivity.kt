eb78362749c30b5e09a78e5bfba2b6702bbaaf0f
Allan Guan <allanzguan@gmail.com>
2020-03-04 18:22:45 -0600

    init code

---
+++ android/app/src/main/kotlin/depaul/team2/qrecyle/MainActivity.kt
@@ -0,0 +1,12 @@
+package depaul.team2.qrecyle
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
